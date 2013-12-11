5c6c62b2e01887b1286e4f97fb16d77e97fad05f
Chad Oliver <hello@chadoliver.co.nz>
2013-12-11 17:13:49 +1300

    tidied up so it fits the pipeline model

--- model/scripts/simulationClock.js
+++ model/scripts/simulationClock.js
@@ -4,77 +4,82 @@
 //    moves faster than sample time, and it can be paused and scrubbed.
 //  - Wall Time is the computer time used to calculate when a javascript event should be triggered.
 
-var SimulationClock = (function () {
-	// SimulationClock is a class which keeps track of the offset between simulation time and real time. You can think 
-	// of SimulationClock as a stopwatch which can be scrubbed and rewound.
+var Clock = (function () {
+	// Clock is the central location for managing the current simulation time. With this class,
+	// all subscribed Time instances can be paused, resumed, and scrubbed at the same time.
 
 	var states = {
 		PAUSED: 'paused',
 		ACTIVE: 'active',
 	};
 
-	var timeEvents = {
-		PAUSE: 'pause',
-		RESUME
-	}
+	function Clock (sampleTimeSpeedup) {
 
-	function Synchron () {
-		// Synchron stores the Simulation Time and Real Time representations of a single instant.
-
-		// Date.now() is just a vaguely reasonable initialisation value. It shouldn't ever be read 
-		// before it is replaced with something better.
-		this.real = Date.now();	
-		this.simulation = 0;
-	}
-
-	function SimulationClock () {
-		this.synchron = new Synchron();
+		this.sampleTimeSpeedup = sampleTimeSpeedup;
+		
+		this.subscribers = [];
 		this.state = states.PAUSED;
-		this.callbacks = [];
+
+		this.synchron = {		// Synchron stores the Simulation Time and Real Time representations of a single instant.
+			real: Date.now(),
+			simulation: 0,
+		};
 	}
 
-	//==== The next three methods form the interface for controlling the set of Time instances. =============================//
+	//===== The Upsteam Interface =================================================================================//
 
-	SimulationClock.prototype.start = function() {
-		// Now may the time start ticking.
+	Clock.prototype.start = function() {
+		// Allow simulation time to begin or resume.
 
 		if (this.state !== states.ACTIVE) {
 			this.synchron.real = Date.now();	// When the clock is paused, simulation time doesn't change but real time does.
 			this.state = states.ACTIVE;
+
+			var offset = this.synchron.real - this.synchron.simulation;
+			for (var i=0; i<this.subscribers.length; i++) {
+				this.subscribers[i].start(this.sampleTimeSpeedup, offset);
+			};
 		}
 	};
 
-	SimulationClock.prototype.pause = function() {
-		// Stop the clock!
+	Clock.prototype.pause = function() {
+		// Pause simulation time.
 
 		if (this.state !== states.PAUSED) {
 			var now = Date.now();
 			this.synchron.simulation += now - this.synchron.real;
 			this.synchron.real = now;
 			this.state = states.PAUSED;
+
+			for (var i=0; i<this.subscribers.length; i++) {
+				this.subscribers[i].pause();
+			};
 		}
 	};
 
-	SimulationClock.prototype.scrub = function(simulationTime) {
+	Clock.prototype.scrub = function(simulationTime) {
 		// Change the current simulation time, without changing the real time. Equivalent to skipping forwards or 
-		// backwards in a movie.
-
-		var stateCopy = this.state;
-		this.pause();					// this is idempotent
+		// backwards in a movie. Note that scrub() won't have any *immediate* effect if this.state is PAUSED.
 
 		this.synchron.simulation = simulationTime;
 
-		if (stateCopy === states.ACTIVE) this.start();
+		if (this.state === states.ACTIVE) {
+			this.synchron.real = Date.now();
 
+			var offset = this.synchron.real - this.synchron.simulation;
+			for (var i=0; i<this.subscribers.length; i++) {
+				this.subscribers[i].start(this.sampleTimeSpeedup, offset);
+			};
+		}
 	};
 
 	//==== The following methods are used by Time instances to respond to control events ====================================//
 
-	SimulationClock.prototype.subscribe = function(callback) {
-		// body...
+	Clock.prototype.subscribe = function(callback) {
+		this.subscribers.push[subscriber];
 	};
 
-	SimulationClock.prototype.getTime = function() {
+	Clock.prototype.getTime = function() {
 		
 		var realOffset = 0;
 		if (this.state === states.ACTIVE) {
@@ -83,12 +88,5 @@ var SimulationClock = (function () {
 		return this.synchron.simulation + realOffset;
 	};
 
-	SimulationClock.prototype.getOffset = function() {
-		if (this.state === states.paused) {
-			this.synchron.real = Date.now();
-		}
-		return this.synchron.real - this.synchron.simulation;
-	};
-
-	return SimulationClock;
+	return Clock;
 })
