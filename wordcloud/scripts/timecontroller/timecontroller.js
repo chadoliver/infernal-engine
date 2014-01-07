97639be9256be35ba377aac0cc1b47045f2f5c59
Chad Oliver <hello@chadoliver.co.nz>
2014-01-08 11:02:51 +1300

    playPauseButton, TimeController, and Clock theoretically working.

--- wordcloud/scripts/timecontroller/timecontroller.js
+++ wordcloud/scripts/timecontroller/timecontroller.js
@@ -10,6 +10,175 @@
 	//===========================================================================================================//
 
 
+	var PlayPauseButton = (function () {
+		// PlayPauseButton models the play/pause button, obviously. TimeController subscribes to this, so that 
+		// pressing the button will toggle the progression of simulationTime throughout the webapp.
+
+		var buttonTypes = {
+			PAUSE: 'pause',
+			PLAY: 'play',
+		};
+	
+		function PlayPauseButton () {
+			
+			this.subscribers = [];
+			this.buttonType = buttonTypes.PLAY;	// time is paused, and the play button is displayed.
+
+			this.element = document.getElementById('play-button');
+			this.element.addEventListener("click", this.toggle.bind(this));
+		}
+
+		PlayPauseButton.prototype.toggle = function() {
+			
+			if (this.buttonType === buttonTypes.PLAY) {
+				this.play();
+			}
+			else if (this.buttonType === buttonTypes.PAUSE) {
+				this.pause();
+			}
+		};
+
+		PlayPauseButton.prototype.play = function() {
+
+			if (this.buttonType !== buttonTypes.PLAY) return;
+
+			for (var i=0; i<this.subscribers.length; i++) {
+				this.subscribers[i].start();
+			};
+			this.buttonType = buttonTypes.PAUSE;
+			// change button image
+		};
+
+		PlayPauseButton.prototype.pause = function() {
+
+			if (this.buttonType !== buttonTypes.PAUSE) return;
+			
+			for (var i=0; i<this.subscribers.length; i++) {
+				this.subscribers[i].pause();
+			};
+			this.buttonType = buttonTypes.PLAY;
+			// change button image
+		};
+
+		PlayPauseButton.prototype.subscribe = function(subscriber) {
+			this.subscribers.push[subscriber];
+		};
+	
+		return PlayPauseButton;
+	})();
+
+
+	//===========================================================================================================//
+
+
+	var TimeController = (function () {
+		// Clock is the central location for managing the current simulation time. With this class,
+		// all subscribed Time instances can be paused, resumed, and scrubbed at the same time.
+
+		var states = {
+			PAUSED: 'paused',
+			ACTIVE: 'active',
+		};
+
+		function TimeController (playPauseButton, sampleTimeSpeedup) {
+
+			this.elements = {
+			    minutes: document.getElementById('minutes'),
+			    seconds: document.getElementById('seconds'),
+			    scrubber: document.getElementById('scrubber'),
+			    start: document.getElementById('start'),
+			    pause: document.getElementById('pause')
+			};
+
+			this.sampleTimeSpeedup = sampleTimeSpeedup || 1;
+			
+			this.subscribers = [];
+			this.state = states.PAUSED;
+
+			this.synchron = {		// Synchron stores the Simulation Time and Real Time representations of a single instant.
+				real: Date.now(),
+				simulation: 0,
+			};
+
+			playPauseButton.subscribe(this);
+		}
+
+		TimeController.prototype.toggle = function() {
+			// start if it's currently paused; pause if it's currently playing.
+
+			if (this.state === states.PAUSED) {
+				this.start();
+			}
+			else if (this.state === states.ACTIVE) {
+				this.pause();
+			}
+		};
+
+		TimeController.prototype.start = function() {
+			// Allow simulation time to begin or resume.
+
+			if (this.state !== states.ACTIVE) {
+				this.synchron.real = Date.now();	// When the clock is paused, simulation time doesn't change but real time does.
+				this.state = states.ACTIVE;
+
+				var offset = this.synchron.real - this.synchron.simulation;
+				for (var i=0; i<this.subscribers.length; i++) {
+					this.subscribers[i].start(this.sampleTimeSpeedup, offset);
+				};
+			}
+		};
+
+		TimeController.prototype.pause = function() {
+			// Pause simulation time.
+
+			if (this.state !== states.PAUSED) {
+				var now = Date.now();
+				this.synchron.simulation += now - this.synchron.real;
+				this.synchron.real = now;
+				this.state = states.PAUSED;
+
+				for (var i=0; i<this.subscribers.length; i++) {
+					this.subscribers[i].pause();
+				};
+			}
+		};
+
+		TimeController.prototype.scrub = function(simulationTime) {
+			// Change the current simulation time, without changing the real time. Equivalent to skipping forwards or 
+			// backwards in a movie. Note that scrub() won't have any *immediate* effect if this.state is PAUSED.
+
+			this.synchron.simulation = simulationTime;
+
+			if (this.state === states.ACTIVE) {
+				this.synchron.real = Date.now();
+
+				var offset = this.synchron.real - this.synchron.simulation;
+				for (var i=0; i<this.subscribers.length; i++) {
+					this.subscribers[i].start(this.sampleTimeSpeedup, offset);
+				};
+			}
+		};
+
+		TimeController.prototype.subscribe = function(subscriber) {
+			this.subscribers.push[subscriber];
+		};
+
+		TimeController.prototype.getTime = function() {
+			
+			var realOffset = 0;
+			if (this.state === states.ACTIVE) {
+				realOffset = Date.now() - this.synchron.real;
+			}
+			return this.synchron.simulation + realOffset;
+		};
+
+		return TimeController;
+	});
+
+	
+	//===========================================================================================================//
+
+
 	var Clock = (function() {
 		// Clock is the class which encapsulates the digital clock in the bottom right of the web page. This 
 		// class is just a View component; it doesn't do anything except update the time on the screen.
@@ -76,114 +245,13 @@
 		return Clock;
 	})();
 
+	var sampleTimeSpeedup = 10;
+	var zeroTime = 60*60;	// 1 hour
 
-	//===========================================================================================================//
-
-
-	var TimeController = (function () {
-		// Clock is the central location for managing the current simulation time. With this class,
-		// all subscribed Time instances can be paused, resumed, and scrubbed at the same time.
-
-		var states = {
-			PAUSED: 'paused',
-			ACTIVE: 'active',
-		};
-
-		function TimeController (sampleTimeSpeedup) {
-
-			this.elements = {
-			    minutes: document.getElementById('minutes'),
-			    seconds: document.getElementById('seconds'),
-			    scrubber: document.getElementById('scrubber'),
-			    start: document.getElementById('start'),
-			    pause: document.getElementById('pause')
-			};
-
-			this.sampleTimeSpeedup = sampleTimeSpeedup;
-			
-			this.subscribers = [];
-			this.state = states.PAUSED;
-
-			this.synchron = {		// Synchron stores the Simulation Time and Real Time representations of a single instant.
-				real: Date.now(),
-				simulation: 0,
-			};
-		}
-
-		//===== The Upsteam Interface =================================================================================//
-
-		TimeController.prototype.toggle = function() {
-			// start if it's currently paused; pause if it's currently playing.
-
-			if (this.state === states.PAUSED) {
-				this.start();
-			}
-			else if (this.state === states.ACTIVE) {
-				this.pause();
-			}
-		};
-
-		TimeController.prototype.start = function() {
-			// Allow simulation time to begin or resume.
-
-			if (this.state !== states.ACTIVE) {
-				this.synchron.real = Date.now();	// When the clock is paused, simulation time doesn't change but real time does.
-				this.state = states.ACTIVE;
-
-				var offset = this.synchron.real - this.synchron.simulation;
-				for (var i=0; i<this.subscribers.length; i++) {
-					this.subscribers[i].start(this.sampleTimeSpeedup, offset);
-				};
-			}
-		};
-
-		TimeController.prototype.pause = function() {
-			// Pause simulation time.
-
-			if (this.state !== states.PAUSED) {
-				var now = Date.now();
-				this.synchron.simulation += now - this.synchron.real;
-				this.synchron.real = now;
-				this.state = states.PAUSED;
-
-				for (var i=0; i<this.subscribers.length; i++) {
-					this.subscribers[i].pause();
-				};
-			}
-		};
-
-		TimeController.prototype.scrub = function(simulationTime) {
-			// Change the current simulation time, without changing the real time. Equivalent to skipping forwards or 
-			// backwards in a movie. Note that scrub() won't have any *immediate* effect if this.state is PAUSED.
-
-			this.synchron.simulation = simulationTime;
-
-			if (this.state === states.ACTIVE) {
-				this.synchron.real = Date.now();
-
-				var offset = this.synchron.real - this.synchron.simulation;
-				for (var i=0; i<this.subscribers.length; i++) {
-					this.subscribers[i].start(this.sampleTimeSpeedup, offset);
-				};
-			}
-		};
-
-		//==== The following methods are used by Time instances to respond to control events ====================================//
-
-		TimeController.prototype.subscribe = function(callback) {
-			this.subscribers.push[subscriber];
-		};
-
-		TimeController.prototype.getTime = function() {
-			
-			var realOffset = 0;
-			if (this.state === states.ACTIVE) {
-				realOffset = Date.now() - this.synchron.real;
-			}
-			return this.synchron.simulation + realOffset;
-		};
-
-		return TimeController;
-	});
+	var playPauseButton = new PlayPauseButton();
+	var timeController = new TimeController(playPauseButton, sampleTimeSpeedup)
+	var clock = new Clock(timeController, zeroTime);
+	
+	playPauseButton.play();
 
 })();
