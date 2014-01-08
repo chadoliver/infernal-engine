4ba37eb44696970873f4b64bbedbfee086a1f2ce
Chad Oliver <hello@chadoliver.co.nz>
2014-01-09 10:32:27 +1300

    extract classes, rename subscribe, rename Time
    - extract action into own file
    - rename subscribe -> registerListener, and associated changes
    - rename Time -> SimulationInstant

--- wordcloud/scripts/action.js
+++ wordcloud/scripts/action.js
@@ -142,14 +142,16 @@
 
 	var Action = (function () {
 		
-		function Action (message, location, personId, sampleTime) {
+		function Action (message, location, personId, sampleTime, timeController) {
 
 			this.message = message;
 			this.location = location;
 			this.personId = personId;
 			this.sampleTime = sampleTime;
 
-			this.simulationInstant = null;
+			this.simulationInstant = new SimulationInstant(this.sampleTime, timeController.zeroTime);
+			timeController.registerListener(this.simulationInstant);
+			this.simulationInstant.registerListener(this);
 
 			this.isActive = false;
 			this.listeners = [];
@@ -186,127 +188,38 @@
 			this.listeners.push(listener);
 		};
 
-		Action.prototype.registerTimeController = function(timeController) {
-			// Now that we've got the time controller, we have everything we need to create a SimulationInstant
-			// and hook up the associated event listeners.
-
-			this.timeController = timeController;
-			this.simulationInstant = new SimulationInstant(this.sampleTime, timeController.zeroTime);
-
-			timeController.registerListener(timeInstant);
-			timeInstant.registerListener(this);
-		};
-
 		return Action;
 	})();
 
-
+	
 	//===========================================================================================================//
 
-
-	var Person = (function () {
-		// the Person class represents people, obviously. Specifically, a Person instance is a container for a set of 
-		// actions.
-
-		function UUID () {
-			return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-			    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-			    return v.toString(16);
-			});
-		};
-
-		function Person (id) {
-			this.id = id || new UUID();
-			this.name = "";
-
-			this.currentPosition = undefined;
-			this.actions = [];
-		}
-
-		Person.prototype.sortActions = function() {
-			// sort actions in this.actions by their timestamp.
-			
-			this.actions.sort( function compare(a, b) {
-				if (a.timestamp < b.timestamp) return -1;
-				if (a.timestamp > b.timestamp) return 1;
-				else return 0;
-			});
-		};
-
-		Person.prototype.registerAction = function(actions) {
-			
-			this.actions.push(action);
-			action.registerListener(this);
-			this.updateOnAction(action);
-		};
-
-		Person.prototype.updateOnAction = function(action) {
-			
-			if (action.isActive) {
-				console.log(action.message.text);
-			};
-		};
-
-		return Person;
-	})();
-
-
-	//===========================================================================================================//
-
-
-	var PersonSet = (function() {
-
-		function PersonSet (timeController, actions) {
-			this.people = [];
-		}
-
-		PersonSet.prototype.getPersonById = function(id) {
-			
-			for (var i=0; i<this.people.length; i++) {
-				var person = this.people[i];
-				if ( person.id === id ) {
-					return person;
-				}
-			}
-
-			// If we get to this point, that means there is no Person instance with that id
-			var person = new Person(id);
-			this.people.push(person);
-			return person;
-		};
-
-		PersonSet.prototype.registerAction = function(action) {
-			
-			var person = this.getPersonById(action.personId);
-			person.registerAction(action);
-		};
-
-		return PersonSet;
-	})();
-
-
-	//===========================================================================================================//
-
-	function begin (timeController, actions) {
+	/*
+	function begin (actions) {
 
 		var personSet = new PersonSet();
-		if (actions.length === undefined) actions = [actions];
 
 		for (var i=0; i<actions.length; i++) {
-			var action = actions[i];
-
-			action.registerTimeController(timeController);
-			personSet.registerAction(action);
+			personSet.registerAction(actions[i]);
 		}
 	}
+	*/
 
-
+	//===========================================================================================================//
 	
+	// The google closure compiler obfuscates the names of classes, properties, and methods. However, it doesn't alter
+	// string literals. We therefore use the following method to ensure that the necessary tokens are available to the
+	// outside world.
 
+	window['Action'] = Action; // <-- Constructor
 	
+	window['Action']['isActive'] = Action.isActive;
+	window['Action']['personId'] = Action.personId;
+	window['Action']['timestamp'] = Action.timestamp;
+	window['Action']['message'] = Action.message;
+	window['Action']['message']['text'] = Action.message.text;
 
-	window['WordCloud'] = WordCloud; // <-- Constructor
-	window['WordCloud'].prototype['paint'] = WordCloud.prototype.paint;
-	window['WordCloud'].prototype['putWord'] = WordCloud.prototype.putWord;
+	window['Action'].prototype['registerListener'] = Action.prototype.registerListener;
+	window['Action'].prototype['updateOnAction'] = Action.prototype.updateOnAction;
 
 })();
