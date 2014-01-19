07a6a396aadc6e2e9ed8aed24fe52fa7de971cba
Chad Oliver <hello@chadoliver.co.nz>
2014-01-20 12:36:39 +1300

    messages are inserted into MessageBoard at the correct location

--- scripts/messageBoard.js
+++ scripts/messageBoard.js
@@ -8,11 +8,12 @@
 			id = id || 'messages';
 
 			this.root = document.getElementById(id);
+			this.messages = []; 	// all elements under this.root have an associated Message object stored in this list.
 			this.listeners = [];
 		}
 
-		MessageBoard.prototype.publishMessage = function(message) {
-			
+		MessageBoard.prototype.createMessageElement = function(message) {
+
 			var wrapperElement = document.createElement('div');
 			var contentElement = document.createElement('div');
 
@@ -23,14 +24,63 @@
 			contentElement.innerHTML = message.name + ": " + message.text;
 
 			wrapperElement.appendChild(contentElement);
-			this.root.appendChild(wrapperElement);
+
+			return wrapperElement;
+		};
+
+		MessageBoard.prototype.findSucceedingMessageElement = function(message) {
+
+			// ensure that this.messages is sorted in chronological order.
+			this.messages.sort( function compare(a, b) {
+				if (a.sampleTime < b.sampleTime) return -1;
+				if (a.sampleTime > b.sampleTime) return 1;
+				else return 0;
+			});
+
+			// find the first message which has a bigger sampleTime (the new message will be inserted
+			// directly before this message)
+			
+			var suceedingMessageElement = null;
+			for (var i=0; i<this.messages.length; i++) {
+				if (this.messages[i].sampleTime > message.sampleTime) {
+					suceedingMessageElement = document.getElementById( this.messages[i].id );
+					break;
+				}
+			}
+			return suceedingMessageElement;
+		};
+
+		MessageBoard.prototype.publishMessage = function(message) {
+			
+			// first, we create the message element (which is actually two elements, one
+			// inside the other).
+			var messageElement = this.createMessageElement(message);
+
+			// now, we need to insert the message element at the right place under this.root.
+			var suceedingMessageElement = this.findSucceedingMessageElement(message);
+			if (suceedingMessageElement === null) {
+				this.root.appendChild(messageElement);
+			} else {
+				this.root.insertBefore(messageElement, suceedingMessageElement);
+			}
+
+			// and finally, we update this.messages so that it includes the new message.
+			this.messages.push(message);
+			
 		};
 
 		MessageBoard.prototype.retractMessage = function(message) {
 			
+			// first, remove the element from under this.root
 			var wrapperElement = document.getElementById(message.id);
 			if (wrapperElement !== null) {
 				this.root.removeChild(wrapperElement);
+			}
+
+			// next, remove the associated message from this.messages.
+			var index = this.messages.indexOf(message);
+			if (index >= 0) {
+				this.messages.splice(index, 1);
 			}
 		};
 
